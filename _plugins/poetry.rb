4e3e1775a0a03f0a6ae02a8a451cb35718196a84
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2022-10-18 21:05:04 -0700

    Cleanup posts/posting system

--- _plugins/poetry.rb
+++ _plugins/poetry.rb
@@ -1,13 +1,55 @@
 module Jekyll
-  class PoetryBlock < Liquid::Block
-    def render(context)
-      text = super
+  class PoemConverter < Converter
+    safe true
+    priority :low
 
-      <<-POEM.chomp
-<article class="poetry">#{text}</article>
-POEM
+    def matches(ext)
+      ext =~ /^\.poem$/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def parse(content)
+      stanzas = []
+      content.chomp.each_line("\n\n") do |par|
+        stanza = []
+        par.chomp.each_line do |line|
+          stanza << line
+        end
+        stanzas << stanza
+      end
+      stanzas
+    end
+
+    def convert(content)
+      output = ""
+      parse(content).each do |stanza|
+        output << "<p>"
+
+        start = true
+        stanza.each do |line|
+          if not start then
+            output << "<br />"
+          end
+          start = false
+          output << line
+        end
+        output << "</p>"
+      end
+      output
     end
   end
 end
 
-Liquid::Template.register_tag('poetry', Jekyll::PoetryBlock)
+class PoetryBlock < Liquid::Block
+  def render(context)
+    text = super
+    <<-POEM.chomp
+<article class="poetry">#{text}</article>
+POEM
+  end
+end
+
+Liquid::Template.register_tag('poetry', PoetryBlock)
