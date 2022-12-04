e82da49d481fe38071ec15543bf9facc3552b3aa
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2022-12-03 16:59:17 -0800

    Fix HTML structure

--- src/_plugins/section_processor.rb
+++ src/_plugins/section_processor.rb
@@ -41,6 +41,9 @@ module Kramdown
         hgroup = Element.new :html_element, 'hgroup'
         hgroup.children.push el
 
+        header = Element.new :html_element, 'header'
+        header.children.push hgroup
+
         if id != nil
         then
           text = Element.new :text
@@ -48,11 +51,8 @@ module Kramdown
 
           a = Element.new :a, nil, {'href' => '#' + id, 'rel' => 'bookmark'}
           a.children = [text]
-          hgroup.children.unshift a
+          header.children.unshift a
         end
-
-        header = Element.new :html_element, 'header'
-        header.children.push hgroup
 
         sect = Section.new level, current, attributes
         sect.children.push header
