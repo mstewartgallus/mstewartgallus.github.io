4cfca95432d5ed19100960f8e18d751fc0343598
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-02-04 17:31:10 -0800

    Cleanup

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -1,57 +1,50 @@
 import RemarkGfm from 'remark-gfm';
 import RehypeSlug from 'rehype-slug';
 
-const feed = {
-    query: `
-{
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-              }
-            }
-          }
-`,
-    feeds: [
-        {
-            serialize: ({ query: { site, allLink } }) => {
-                const siteUrl = site.siteMetadata.siteUrl;
-                return allLink.nodes.map(node => {
-                    const { metadata } = node.post;
-                    return {
-                        ...metadata,
-                        url: siteUrl + metadata.slug,
-                        guid: siteUrl + metadata.slug,
-                        custom_elements: []
-                    };
-                });
-            },
-            query: `
-              {
-                allLink(sort: { date: DESC }) {
-                  nodes {
-                    post {
-                      metadata {
-                        title
-                        date
-                        slug
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: "/feed.xml",
-            title: "Words to Kick Your Teeth Out"
-        }
-    ]
-};
 
 export const siteMetadata = {
     title: "Words to Kick Your Teeth Out",
     description: "lol lmao",
     siteUrl: "https://mstewartgallus.github.io"
+};
+
+const serialize = ({ query: { allLink } }) => {
+    const siteUrl = siteMetadata.siteUrl;
+    return allLink.nodes.map(node => {
+        const { metadata } = node.post;
+        return {
+            ...metadata,
+            url: siteUrl + metadata.slug,
+            guid: siteUrl + metadata.slug,
+            custom_elements: []
+        };
+    });
+};
+
+const query =
+`{
+  allLink(sort: { date: DESC }) {
+   nodes {
+     post {
+       metadata {
+         title
+         date
+         slug
+       }
+     }
+   }
+  }
+}`;
+
+const feed = {
+    feeds: [
+        {
+            serialize,
+            query,
+            output: "/feed.xml",
+            title: siteMetadata.title
+        }
+    ]
 };
 
 export const graphqlTypegen = true;
@@ -66,10 +59,10 @@ export const plugins = [
         resolve: "gatsby-plugin-mdx",
         options: {
             extensions: ['.md', '.mdx', '.markdown'],
-            //  mdxOptions: {
-            //      remarkPlugins: [RemarkGfm],
-            //      rehypePlugins: [RehypeSlug],
-            // }
+             mdxOptions: {
+                 remarkPlugins: [RemarkGfm],
+                 rehypePlugins: [RehypeSlug],
+            }
         }
     },
     "gatsby-transformer-yaml",
