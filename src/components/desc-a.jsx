76761d67ede9f967685d012d40e42d7d6f221c3b
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-02-23 16:24:34 -0800

    fix description links

--- src/components/desc-a.jsx
+++ src/components/desc-a.jsx
@@ -3,8 +3,8 @@ import A from "./a.jsx";
 
 const DescA = ({ children, desc, ...props }) => {
     const id = React.useId();
-    return <A {...props}>
-               <span aria-describedby={id}>{children}</span>
+    return <A {...props} aria-describedby={id}>
+               {children}
                &emsp;
                <span id={id} aria-hidden="true">{desc}</span>
            </A>;
