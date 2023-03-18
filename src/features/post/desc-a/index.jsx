4d3c2d247ec6a0f4f04e4e3d46f5a87568283f1b
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-03-18 16:56:31 -0700

    Cleanup anchor description

--- src/features/post/desc-a/index.jsx
+++ src/features/post/desc-a/index.jsx
@@ -1,31 +1,19 @@
 import { useId, forwardRef } from "react";
 import { A } from "../../../features/ui";
 import {
-    grid as gridClass,
-    real as realClass,
-    fake as fakeClass,
-    desc as descClass
+    grid as gridClass
 } from "./desc-a.module.css";
 
 const DescAImpl = ({ children, desc, ...props }, ref) => {
     const id = useId();
-    return <dl className={gridClass} role="presentation">
-               <div>
-                   <dt className={realClass} role="presentation">
-                       <A aria-describedby={id} ref={ref} {...props}>
-                           {children}
-                       </A>
-                   </dt>
-                   <dd className={fakeClass} aria-hidden="true">
-                       <A {...props}>
-                           {children}
-                       </A>
-                   </dd>
-                   <dd id={id} className={descClass} role="presentation">
+    return <div aria-owns={id} role="group">
+               <A className={gridClass} aria-describedby={id} ref={ref} {...props}>
+                   <span role="presentation">{children}</span>
+                   <span id={id} role="presentation">
                        {desc}
-                   </dd>
-               </div>
-           </dl>;
+                   </span>
+               </A>
+           </div>;
 };
 
 export const DescA = forwardRef(DescAImpl);
