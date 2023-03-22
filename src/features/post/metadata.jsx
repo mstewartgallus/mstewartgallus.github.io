e9db64cfc3cf7a4c7ff87d71de45b6b0452ce49d
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-03-22 14:17:21 -0700

    Cleanup desc list

--- src/features/post/metadata.jsx
+++ src/features/post/metadata.jsx
@@ -2,28 +2,28 @@ import { createContext, useId, useContext } from "react";
 import { useSearchURL } from "../../features/route";
 import { A } from "../../features/ui";
 import { DescA } from "./desc-a";
-import { dl, dt, dd } from "./metadata.module.css";
+import { dl, dt, list, item } from "./metadata.module.css";
 
 const DescContext = createContext(null);
 
 const Desc = ({desc,children}) => {
     const id = useId();
-    return <div className={dl}>
-               <dt className={dt} id={id}>{desc}</dt>
-               <DescContext.Provider value={id}>
-                   {children}
-               </DescContext.Provider>
+    return <div role="presentation" className={dl}>
+               <div role="presentation" className={dt} id={id}>{desc}</div>
+               <ul role="list" aria-labelledby={id} className={list}>
+                   <DescContext.Provider value={id}>
+                       {children}
+                   </DescContext.Provider>
+               </ul>
            </div>;
 };
 
-const Item = ({
-    children,
-    ...props
-}) => {
+const Item = ({ children, ...props }) => {
     const id = useContext(DescContext);
-    return <dd className={dd}>
+    // work around Safari silliness
+    return <li role="listitem" className={item}>
                <A aria-describedby={id} {...props}>{children}</A>
-           </dd>;
+           </li>;
 };
 
 const AnItem = ({children, filter, item, ...props}) => {
@@ -47,56 +47,57 @@ export const Metadata = ({
                              </time>
           </div>
           <DescA rel="author" href={author.url} desc={author.name}>Author</DescA>
-          <dl style={{margin:0}}>
-              {
-                  places && places.length > 0 &&
-                      <Desc desc="Place">
-                          {
-                              places.map(item =>
-                                  <AnItem
-                                      filter="place"
-                                      rel="tag"
-                                      item={item}
-                                  >
-                                      {item}
-                                  </AnItem>
-                              )
-                          }
-                      </Desc>
-              }
-              {
-                  tags && tags.length > 0 &&
-                      <Desc desc="Tag">
-                          {
-                              tags.map(item =>
-                                  <AnItem
-                                      filter="tag"
-                                      rel="tag"
-                                      item={item}
-                                  >
-                                      {item}
-                                  </AnItem>
-                              )
-                          }
-                      </Desc>
-              }
-              {
-                  people && people.length > 0 &&
-                      <Desc desc="Person">
-                          {
-                              people.map(item =>
-                                  <AnItem
-                                      filter="person"
-                                      rel="tag"
-                                      item={item}
-                                  >
-                                      {item}
-                                  </AnItem>
-                              )
-                          }
-                      </Desc>
-              }
-          </dl>
+          {
+              places && places.length > 0 &&
+                  <Desc desc="Place">
+                      {
+                          places.map(item =>
+                              <AnItem
+                                  key={item}
+                                  filter="place"
+                                  rel="tag"
+                                  item={item}
+                              >
+                                  {item}
+                              </AnItem>
+                          )
+                      }
+                  </Desc>
+          }
+          {
+              tags && tags.length > 0 &&
+                  <Desc desc="Tag">
+                      {
+                          tags.map(item =>
+                              <AnItem
+                                  key={item}
+                                  filter="tag"
+                                  rel="tag"
+                                  item={item}
+                              >
+                                  {item}
+                              </AnItem>
+                          )
+                      }
+                  </Desc>
+          }
+          {
+              people && people.length > 0 &&
+                  <Desc desc="Person">
+                      {
+                          people.map(item =>
+                              <AnItem
+                                  key={item}
+                                  filter="person"
+                                  rel="tag"
+                                  item={item}
+                              >
+                                  {item}
+                              </AnItem>
+                          )
+                      }
+                  </Desc>
+          }
       </div>;
 
 export default Metadata;
