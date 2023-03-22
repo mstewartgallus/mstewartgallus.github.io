04bba7b7f1f2f910ec4068162d3b74e6052ee5b3
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-03-21 17:58:33 -0700

    Cleanup

--- src/features/post/metadata.jsx
+++ src/features/post/metadata.jsx
@@ -1,71 +1,102 @@
+import { createContext, useId, useContext } from "react";
 import { useSearchURL } from "../../features/route";
 import { A } from "../../features/ui";
 import { DescA } from "./desc-a";
-import { DescList, DescItem } from "./desc-list";
+import { dl, dt, dd } from "./metadata.module.css";
 
-const TagA = ({tag}) => {
-    const href = useSearchURL({tag: [tag]});
-    return <A href={href} rel="tag" data-pagefind-filter="tag">{tag}</A>;
+const DescContext = createContext(null);
+
+const Desc = ({desc,children}) => {
+    const id = useId();
+    return <div className={dl}>
+               <dt className={dt} id={id}>{desc}</dt>
+               <DescContext.Provider value={id}>
+                   {children}
+               </DescContext.Provider>
+           </div>;
 };
 
-const PersonA = ({person}) => {
-    const href = useSearchURL({ person: [person] });
-    return <A href={href} rel="tag" data-pagefind-filter="person">{person}</A>;
+const Item = ({
+    children,
+    ...props
+}) => {
+    const id = useContext(DescContext);
+    return <dd className={dd}>
+               <A aria-describedby={id} {...props}>{children}</A>
+           </dd>;
 };
 
-const PlaceA = ({place}) => {
-    const href = useSearchURL({ place: [place] });
-    return <A href={href} rel="tag" data-pagefind-filter="place">{place}</A>;
+const AnItem = ({children, filter, item, ...props}) => {
+    const href = useSearchURL({ [filter]: [item] });
+    return <Item
+               href={href}
+               data-pagefind-filter={filter}
+               {...props}>{children}</Item>;
 };
 
-const Places = ({places}) => places.map(p =>
-    <DescItem key={p}>
-        <PlaceA place={p}/>
-    </DescItem>
-);
-
-const Tags = ({tags}) => tags.map(t =>
-    <DescItem key={t}>
-        <TagA tag={t}/>
-    </DescItem>
-);
-
-const People = ({people}) => people.map(p =>
-    <DescItem key={p}>
-        <PersonA person={p}/>
-    </DescItem>
-);
-
-const PlaceList = ({places}) =>
-<DescList desc="Place">
-    <Places places={places} />
-</DescList>;
-
-const TagList = ({tags}) =>
-<DescList desc="Tag">
-    <Tags tags={tags} />
-</DescList>;
-
-const PeopleList = ({people}) =>
-<DescList desc="People">
-    <People people={people} />
-</DescList>;
+ // items && items.length > 0 &&
 
 export const Metadata = ({
     dateDisplay, date, author, places, tags, people
-}) =>
-<div>
-    <div>
-        Post Date&emsp;<time data-pagefind-filter="date[datetime]"
-                      data-pagefind-sort="date[datetime]"
-                      dateTime={date}>
-                    {dateDisplay}
-                </time>
-    </div>
-    <DescA rel="author" href={author.url} desc={author.name}>Author</DescA>
-    { places && places.length > 0 && <PlaceList places={places} /> }
-    { tags && tags.length > 0 && <TagList tags={tags} /> }
-    { people && people.length > 0 && <PeopleList people={people} /> }
-</div>;
+}) => <div>
+          <div>
+              Post Date&emsp;<time data-pagefind-filter="date[datetime]"
+                                   data-pagefind-sort="date[datetime]"
+                                   dateTime={date}>
+                                 {dateDisplay}
+                             </time>
+          </div>
+          <DescA rel="author" href={author.url} desc={author.name}>Author</DescA>
+          <dl style={{margin:0}}>
+              {
+                  places && places.length > 0 &&
+                      <Desc desc="Place">
+                          {
+                              places.map(item =>
+                                  <AnItem
+                                      filter="place"
+                                      rel="tag"
+                                      item={item}
+                                  >
+                                      {item}
+                                  </AnItem>
+                              )
+                          }
+                      </Desc>
+              }
+              {
+                  tags && tags.length > 0 &&
+                      <Desc desc="Tag">
+                          {
+                              tags.map(item =>
+                                  <AnItem
+                                      filter="tag"
+                                      rel="tag"
+                                      item={item}
+                                  >
+                                      {item}
+                                  </AnItem>
+                              )
+                          }
+                      </Desc>
+              }
+              {
+                  people && people.length > 0 &&
+                      <Desc desc="Person">
+                          {
+                              people.map(item =>
+                                  <AnItem
+                                      filter="person"
+                                      rel="tag"
+                                      item={item}
+                                  >
+                                      {item}
+                                  </AnItem>
+                              )
+                          }
+                      </Desc>
+              }
+          </dl>
+      </div>;
 
 export default Metadata;
