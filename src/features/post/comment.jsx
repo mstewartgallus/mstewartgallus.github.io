b1d03eea5b6f7ca7174fe12c0e45d4b2122808a7
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-03-22 20:33:49 -0700

    Cleanup

--- src/features/post/comment.jsx
+++ src/features/post/comment.jsx
@@ -37,7 +37,7 @@ const Comment = ({url, account, content, children}) => {
 
     const loadClass = loaded ? loadedClass : unloadedClass;
 
-    return <article className={commentClass}>
+    return <article className={commentClass} role="comment">
                <header className={headerClass}>
                    <Account {...account} />
                    <A rel="nofollow" href={url}>#</A>
