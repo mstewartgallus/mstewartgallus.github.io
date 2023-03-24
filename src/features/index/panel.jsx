ebfde11641a41a46ab995807fc32d1c2abeae0b7
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-03-23 19:57:04 -0700

    Tweak and leanup accordion

--- src/features/index/panel.jsx
+++ src/features/index/panel.jsx
@@ -1,6 +1,6 @@
 import { useId, useTransition, useCallback, createContext, useContext } from "react";
 import { Card, H2 } from "../../features/ui";
-import { section, button } from "./panel.module.css";
+import { state, button } from "./panel.module.css";
 
 const Context = createContext(null);
 
@@ -28,19 +28,20 @@ export const Panel = ({children, heading, value, onClick}) => {
     const open = selected === value;
 
     return <Card>
-               <H2>
-                   <button onClick={onClickWrapper}
-                           className={button}
+               <H2 id={titleId} onClick={onClickWrapper}>
+                   <button className={button}
                            aria-controls={contentId}
                            aria-expanded={String(open)}>
-                       <span id={titleId}>{heading}</span>
+                       <span aria-hidden="true" className={state} data-open={String(open)}>
+                           &ensp;
+                       </span>
+                       {heading}
                    </button>
                </H2>
-               <section id={contentId}
-                        aria-labelledby={titleId}
-                        className={section}
-                        data-open={String(open)}>
+               <nav id={contentId}
+                    aria-labelledby={titleId}
+                    hidden={open ? null : "hidden"}>
                    {children}
-               </section>
+               </nav>
            </Card>;
 };
