f5bb1969105ac9a784d53857836174d36e3170db
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-03-30 09:14:57 -0700

    Cleanup

--- src/features/mdx/pre/index.jsx
+++ src/features/mdx/pre/index.jsx
@@ -1,20 +1,21 @@
 import { useId } from "react"
-import { Pre as BarePre } from "../../../features/ui";
-import { ScrollArticle } from "../scroll-article";
-import { figure, figcaption } from "./pre.module.css";
+import { Pre as BarePre, Card } from "../../../features/ui";
+import { scroller, figure, figcaption } from "./pre.module.css";
 
 export const Pre = ({children, id, title}) => {
     const caption = useId();
-    return <figure id={id} className={figure}>
-               {title &&
-                <figcaption id={caption} className={figcaption}>{title}</figcaption>
-               }
-               <ScrollArticle aria-labelledby={title && caption}>
-                   <BarePre>
-                       {children}
-                   </BarePre>
-               </ScrollArticle>
-           </figure>;
+    return <Card>
+               <section tabIndex="0" id={id} aria-labelledby={title && caption} className={scroller}>
+                   <figure className={figure}>
+                       {title &&
+                        <figcaption id={caption} className={figcaption}>{title}</figcaption>
+                       }
+                       <BarePre>
+                           {children}
+                       </BarePre>
+                   </figure>
+               </section>
+           </Card>;
 };
 
 export default Pre;
--- src/features/mdx/scroll-article/index.jsx
+++
@@ -1,9 +0,0 @@
-import { scroller } from "./scroll-article.module.css";
-
-export const ScrollArticle = ({children, ...props}) => {
-    return <article tabIndex="0" className={scroller} {...props}>
-               {children}
-           </article>;
-};
-
-export default ScrollArticle;
