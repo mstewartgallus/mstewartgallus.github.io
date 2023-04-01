5b0b87d540ea2ba26420c570f48d9dcd512ff915
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-04-01 12:00:26 -0700

    Hack around gatsbyjs focus management

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,7 @@
 import 'wicg-inert';
 import "./src/assets/index.css";
+
+// hack around the gatsby focus wrapper for manual focus management
+export const onClientEntry = () => {
+    document.getElementById('gatsby-focus-wrapper').removeAttribute('tabIndex');
+};
