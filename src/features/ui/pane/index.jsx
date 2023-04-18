5e3a717ec904cb46928b63ca3c3e2631608e37e9
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-04-18 15:49:37 -0700

    Cleanup accordion animation

--- src/features/ui/pane/index.jsx
+++ src/features/ui/pane/index.jsx
@@ -1,28 +1,34 @@
 import { useDeferredValue } from "react";
 import { useClient } from "@features/util";
 import {
-    disclosure, disclosureHidden, disclosureWillChange,
-    wrapper, wrapperHidden, wrapperWillChange,
-    content, contentHidden, contentWillChange
+    disclosure, disclosureServer, disclosureClient, disclosureHidden, disclosureWillChange,
+    wrapper, wrapperServer, wrapperClient, wrapperHidden, wrapperWillChange,
+    content, contentServer, contentClient, contentHidden, contentWillChange
 } from "./panel.module.css";
 
 export const Pane = ({children, willChange, open}) => {
     const client = useClient();
-    const deferredWillChange = useDeferredValue(willChange);
-    const deferredOpen = useDeferredValue(open || !client);
+    let deferredWillChange = useDeferredValue(willChange);
+    let deferredOpen = useDeferredValue(open);
+
+    deferredWillChange &&= client;
+    deferredOpen ||= !client;
 
     const disclosureClass = [
         disclosure,
+        client ? disclosureClient : disclosureServer,
         deferredWillChange ? disclosureWillChange : '',
         deferredOpen ? '' : disclosureHidden
     ].join(' ');
     const wrapperClass = [
         wrapper,
+        client ? wrapperClient : wrapperServer,
         deferredWillChange ? wrapperWillChange : '',
         deferredOpen ? '' : wrapperHidden
     ].join(' ');
     const contentClass = [
         content,
+        client ? contentClient : contentServer,
         deferredWillChange ? contentWillChange : '',
         deferredOpen ? '' : contentHidden
     ].join(' ');
