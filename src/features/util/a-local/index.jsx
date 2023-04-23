480a88c8d894e0552c0e5a06277d05cf2308cd66
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-04-22 17:19:10 -0700

    Cleanup

--- src/features/util/a-local/index.jsx
+++ src/features/util/a-local/index.jsx
@@ -1,80 +1,85 @@
-import {
-    forwardRef,
-    useCallback
-} from "react";
+import { forwardRef, useCallback } from "react";
 import { navigate } from "gatsby";
 import { useA } from "./use-a";
 import { useHover } from "./use-hover";
 import { usePrefetchPathname } from "./use-prefetch-pathname";
 import { useNear } from "./use-intersect";
 
-const ALocal = (props, ref) => {
+const useOnClick = (href, onClick) => useCallback(async e => {
+    onClick?.(e);
+
+    const { defaultPrevented, altKey, metaKey, shiftKey, ctrlKey, button } = e;
+    if (defaultPrevented) {
+        return;
+    }
+    if (button !== 0) {
+        return;
+    }
+
+    if (altKey || metaKey || shiftKey || ctrlKey) {
+        return;
+    }
+
+    e.preventDefault();
+    await navigate(href);
+}, [onClick, href]);
+
+const useOnMouseOver = (mouseOver, onMouseOver) => useCallback(e => {
+    mouseOver();
+    onMouseOver?.(e);
+}, [onMouseOver, mouseOver]);
+
+const useOnMouseOut = (mouseOut, onMouseOut) => useCallback(e => {
+    mouseOut();
+    onMouseOut?.(e);
+}, [onMouseOut, mouseOut]);
+
+const useOnFocus = (focus, onFocus) => useCallback(e => {
+    focus();
+    onFocus?.(e);
+}, [onFocus, focus]);
+
+const useOnBlur = (blur, onBlur) => useCallback(e => {
+    blur();
+    onBlur?.(e);
+}, [onBlur, blur]);
+
+const ALocal = (props, theRef) => {
     const {
+        href,
         onClick,
         onFocus, onBlur,
         onMouseOver, onMouseOut
     } = props;
 
-    const { href } = props;
-
-    const onClickWrapper = useCallback(async e => {
-        onClick?.(e);
-
-        const { defaultPrevented, altKey, metaKey, shiftKey, ctrlKey, button } = e;
-        if (defaultPrevented) {
-            return;
-        }
-        if (button !== 0) {
-            return;
-        }
-
-        if (altKey || metaKey || shiftKey || ctrlKey) {
-            return;
-        }
-
-        e.preventDefault();
-        await navigate(href);
-    }, [onClick, href]);
-
     const { hover, focus, blur, mouseOver, mouseOut } = useA();
 
-    const onMouseOverWrapper = useCallback(e => {
-        mouseOver();
-        onMouseOver?.(e);
-    }, [onMouseOver, mouseOver]);
-    const onMouseOutWrapper = useCallback(e => {
-        mouseOut();
-        onMouseOut?.(e);
-    }, [onMouseOut, mouseOut]);
+    const onClickWrap = useOnClick(href, onClick);
+    const onMouseOverWrap = useOnMouseOver(mouseOver, onMouseOver);
+    const onMouseOutWrap = useOnMouseOut(mouseOut, onMouseOut);
+    const onFocusWrap = useOnFocus(focus, onFocus);
+    const onBlurWrap = useOnBlur(blur, onBlur);
 
-    const onFocusWrapper = useCallback(e => {
-        focus();
-        onFocus?.(e);
-    }, [onFocus, focus]);
-    const onBlurWrapper = useCallback(e => {
-        blur();
-        onBlur?.(e);
-    }, [onBlur, blur]);
+    useHover(hover ? href : null);
 
     const { ref: prefetchRef, near } = useNear();
 
-    useHover(hover ? href : null);
+    const ref = useCallback(elem => elem => {
+        prefetchRef(elem);
+        if (theRef) {
+            theRef.current = elem;
+        }
+    }, [theRef, prefetchRef]);
+
     usePrefetchPathname((near || hover) ? href : null);
 
-    return <a
-               {...props}
-               onClick={onClickWrapper}
-               onMouseOver={onMouseOverWrapper}
-               onMouseOut={onMouseOutWrapper}
-               onFocus={onFocusWrapper}
-               onBlur={onBlurWrapper}
-               ref={elem => {
-                   prefetchRef(elem);
-                   if (ref) {
-                       ref.current = elem;
-                   }
-               }}
-           />;
+    return <a {...props}
+              onClick={onClickWrap}
+              onMouseOver={onMouseOverWrap}
+              onMouseOut={onMouseOutWrap}
+              onFocus={onFocusWrap}
+              onBlur={onBlurWrap}
+              ref={ref} />;
 };
 
 const ARef = forwardRef(ALocal);
