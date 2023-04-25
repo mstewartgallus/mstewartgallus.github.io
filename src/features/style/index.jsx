3d1c83c5c933560e6955a98c953a0155b30d3ee2
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-04-25 15:32:08 -0700

    Prefetching

--- src/features/style/index.jsx
+++ src/features/style/index.jsx
@@ -1,6 +1,7 @@
 import favicon from './favicon.svg';
 
 const HeadComponents = [
+    <link rel="preload" href="/page-data/app-data.json" as="fetch" crossOrigin="anonymous" />,
     <meta name="color-scheme" content="dark light" />,
     <meta name="theme-color" content="#6000A0" />,
     <link rel="icon" href={favicon} />
