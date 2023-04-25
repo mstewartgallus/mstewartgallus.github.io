ee4b55ce16ee19b6a905c125b2dd4339f070d9af
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-04-24 17:52:49 -0700

    Rm back to top anchor

--- src/features/page/viewport-page/index.jsx
+++ src/features/page/viewport-page/index.jsx
@@ -1,9 +1,8 @@
 import { ScreenOnly } from "@features/util";
-import { A, Card, Column, H1, H2, Hgroup, Nav, Theme } from "@features/ui";
+import { Card, Column, H1, H2, Hgroup, Nav, Theme } from "@features/ui";
 import { Page } from "../page";
 import { SidebarLayout } from "../sidebar-layout";
 import { SkipA } from "../skip-a";
-import { footer as footerClass } from "./page.module.css";
 
 export const ViewportPage = ({
     children,
@@ -16,42 +15,35 @@ export const ViewportPage = ({
 }) =>
 <Theme>
     <Page>
-        <Column>
-            <SidebarLayout
-                sidebar={
-                    <Column>
-                        {sidebar}
-                        <ScreenOnly>
-                            <Nav heading={<H2>Breadcrumbs</H2>}>
-                                {breadcrumbs}
-                            </Nav>
-                        </ScreenOnly>
-                    </Column>
-                }
-            >
+        <SidebarLayout
+            sidebar={
                 <Column>
-                    <main data-pagefind-body="" aria-describedby="content">
-                        <Card>
-                            <header>
-                                <Hgroup>
-                                    <H1>
-                                        <SkipA id="content" href="#content">{heading}</SkipA>
-                                    </H1>
-                                    {subheading}
-                                </Hgroup>
-                                {notice}
-                            </header>
-                            {children}
-                        </Card>
-                    </main>
-                    {mainbar}
+                    {sidebar}
+                    <ScreenOnly>
+                        <Nav heading={<H2>Breadcrumbs</H2>}>
+                            {breadcrumbs}
+                        </Nav>
+                    </ScreenOnly>
                 </Column>
-            </SidebarLayout>
-            <div className={footerClass}>
-                <Card>
-                    <A href="#content">Back to Top</A>
-                </Card>
-            </div>
-        </Column>
+            }
+        >
+            <Column>
+                <main data-pagefind-body="" aria-describedby="content">
+                    <Card>
+                        <header>
+                            <Hgroup>
+                                <H1>
+                                    <SkipA id="content" href="#content">{heading}</SkipA>
+                                </H1>
+                                {subheading}
+                            </Hgroup>
+                            {notice}
+                        </header>
+                        {children}
+                    </Card>
+                </main>
+                {mainbar}
+            </Column>
+        </SidebarLayout>
     </Page>
 </Theme>;
