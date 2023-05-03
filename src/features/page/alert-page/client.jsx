bbea17366f61e7427e16122248cc6f88c5fc7ae5
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-05-03 08:50:08 -0700

    rm undefined

--- src/features/page/alert-page/client.jsx
+++ src/features/page/alert-page/client.jsx
@@ -1,6 +1,7 @@
 import { useCallback, useRef, useEffect } from "react";
+import { RootScroller } from "@features/root-scroller";
 import { navigate } from "@features/util";
-import { H1, RootScroller, Button, Card, Hgroup } from "@features/ui";
+import { H1, Button, Card, Hgroup, Theme } from "@features/ui";
 import { dialog, header } from "./alert.module.css";
 
 const Dialog = ({children, ...props}) => {
@@ -34,23 +35,25 @@ const Dialog = ({children, ...props}) => {
 };
 
 const AlertPageClient = ({children, heading}) =>
-<RootScroller>
-    <main aria-labelledby="content">
-        <Dialog role="alertdialog"
-                aria-labelledby="content">
-            <Card>
-                <header className={header}>
-                    <Hgroup>
-                        <H1>{heading}</H1>
-                    </Hgroup>
-                    <form method="dialog">
-                        <Button value="back">Back</Button>
-                    </form>
-                </header>
-                {children}
-            </Card>
-        </Dialog>
-    </main>
-</RootScroller>;
+<Theme>
+    <RootScroller>
+        <main aria-labelledby="content">
+            <Dialog role="alertdialog"
+                    aria-labelledby="content">
+                <Card>
+                    <header className={header}>
+                        <Hgroup>
+                            <H1>{heading}</H1>
+                        </Hgroup>
+                        <form method="dialog">
+                            <Button value="back">Back</Button>
+                        </form>
+                    </header>
+                    {children}
+                </Card>
+            </Dialog>
+        </main>
+    </RootScroller>
+</Theme>;
 
 export default AlertPageClient;
