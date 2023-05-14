91ff77cc6e4ea4142790df44e5e665ea0d481b9c
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2023-05-14 14:15:14 -0700

    Cleanup

--- src/features/root-scroller/root-scroller.jsx
+++ src/features/root-scroller/root-scroller.jsx
@@ -1,52 +1,26 @@
-import { useContext, useCallback, useRef } from "react";
+import { useContext, useRef } from "react";
 import { useLocation } from "@features/location";
 import { Context } from "./context.js";
 import { scroller } from "./scroller.module.css";
 
-const useThrottle = () => {
-    const dirty = useRef(false);
-    return useCallback((ival, cb) => {
-        if (dirty.current) {
-            return;
-        }
-        dirty.current = true;
-
-        setTimeout(() => {
-            cb();
-            dirty.current = false;
-        }, ival);
-    }, []);
-};
-
 export const RootScroller = ({children}) => {
-    const { scroll, setScroll } = useContext(Context);
+    const { scrollLeft, scrollTop, onScroll } = useContext(Context);
     const { hash } = useLocation();
 
-    const ref = useRef(null);
-    const scrollRef = useRef({ scroll, hash });
-
-    const throttle = useThrottle();
-    const onScroll = useCallback(() => {
-        throttle(100, () => {
-            const { current } = ref;
-            if (!current) {
-                return;
-            }
-            setScroll(current.scrollLeft, current.scrollTop);
-        });
-    }, [throttle, setScroll]);
-
-    const cb = useCallback(elem => {
-        ref.current = elem;
+    const { current: cb } = useRef(elem => {
         // FIXME hash?
-        const {scroll, hash} = scrollRef.current;
-        if (hash || !scroll || !elem) {
+        if (hash || !elem) {
             return;
         }
-        const { left, top } = scroll;
-        queueMicrotask(() =>
-            elem.scrollTo({ left, top, behaviour: 'instant' }));
-    }, []);
+
+        queueMicrotask(() => {
+            elem.scrollTo({
+                left: scrollLeft,
+                top: scrollTop,
+                behaviour: 'instant'
+            });
+        });
+    });
 
     return <div className={scroller} onScroll={onScroll} ref={cb}>
                {children}
--- src/features/root-scroller/scroll-provider.jsx
+++ src/features/root-scroller/scroll-provider.jsx
@@ -1,13 +1,42 @@
-import { useCallback, useState, useTransition } from "react";
+import { useCallback, useRef, useState, useTransition } from "react";
 import { Context } from "./context.js";
 
 const { Provider } = Context;
 
+const useThrottle = () => {
+    const dirty = useRef(false);
+    return useCallback((ival, cb) => {
+        if (dirty.current) {
+            return;
+        }
+        dirty.current = true;
+
+        setTimeout(() => {
+            cb();
+            dirty.current = false;
+        }, ival);
+    }, []);
+};
+
+const initialState = {
+    scrollLeft: 0,
+    scrollTop: 0
+};
+
 export const ScrollProvider = ({children}) => {
     const [, startTransition] = useTransition();
-    const [scroll, set] = useState(null);
-    const setScroll = useCallback((left, top) => startTransition(() => set({top, left})), []);
-    return <Provider value={{ scroll, setScroll }}>
+    const [{ scrollLeft, scrollTop }, set] = useState(initialState);
+
+    const throttle = useThrottle();
+    const onScroll = useCallback(e => {
+        const { target } = e;
+        throttle(100, () => {
+            const { scrollLeft, scrollTop } = target;
+            startTransition(() => set({scrollLeft, scrollTop}));
+        });
+    }, [throttle]);
+
+    return <Provider value={{ scrollLeft, scrollTop, onScroll }}>
                {children}
            </Provider>;
 };
