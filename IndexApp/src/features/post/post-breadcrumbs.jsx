e80872e6bdcd38ae107722fa25d570f7427d83aa
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2024-07-04 11:22:29 -0700

    Fixup breadcrumbs

--- IndexApp/src/features/post/post-breadcrumbs.jsx
+++ IndexApp/src/features/post/post-breadcrumbs.jsx
@@ -4,7 +4,7 @@ import { BreadcrumbList, BreadcrumbItem, BreadcrumbA, Cite } from "@features/ui"
 export const PostBreadcrumbs = ({category, title}) => {
     const cat = useSearchURL({ category: [category] });
     return <BreadcrumbList>
-               <BreadcrumbA href="/">Home</BreadcrumbA>
+               <BreadcrumbA href="/blog">Blog</BreadcrumbA>
                <BreadcrumbA rel="tag" href={cat}>{category}</BreadcrumbA>
                <BreadcrumbItem>
                    <Cite aria-current="page">{title}</Cite>
