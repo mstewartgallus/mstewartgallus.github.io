b50803bf32b793cc18b78f6ef3dfd9b36bf4e443
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2025-07-07 16:38:08 -0700

    Work on prototyping todo app more

--- IndexApp/src/pages/ten.jsx
+++ IndexApp/src/pages/ten.jsx
@@ -1,53 +1,115 @@
-import { title, entry, itemset, items, item, skip } from "./ten.module.css";
+import { useId, useState, useCallback, useContext, useMemo, createContext } from 'react';
+import { title,
+         itemContent, itemContentInner,
+         items, item, skip } from "./ten.module.css";
 
 const ids = ['foo', 'bar', 'gar', 'dar'];
 const archived = ['foo', 'bar', 'gar', 'dar'];
 
+const SelectionContext = createContext({ selection: -1, setSelection: () => {}});
+const OpenContext = createContext({ open: false, setOpen: () => {}});
+
+const Accordion = ({children}) => {
+    const [selection, setSelection] = useState(null);
+    return <ul>
+               <SelectionContext.Provider value={{ selection, setSelection }}>
+                   {children}
+               </SelectionContext.Provider>
+           </ul>;
+};
+
+const AccordionItem = ({children}) => {
+    const id = useId();
+    const { selection, setSelection } = useContext(SelectionContext);
+    const setOpen = useCallback(open => {
+        setSelection(open ? id : -1);
+    }, [setSelection, id]);
+    const open = useMemo(() => selection === id, [selection, id]);
+    return <li>
+               <OpenContext.Provider value={{ open, setOpen }}>
+                   {children}
+               </OpenContext.Provider>
+           </li>;
+};
+
+const AccordionItemSummary = ({children}) => {
+    const id = useId();
+    const { open, setOpen } = useContext(OpenContext);
+    const onClick = useCallback(() => {
+        setOpen(!open);
+    }, [open, setOpen]);
+    return <div>
+               <label for={id}>{children}</label>
+               <button id={id} onClick={onClick}>
+                   {
+                       open ? <>Open</> : <>Closed</>
+                   }
+               </button>
+           </div>;
+};
+
+const AccordionItemContent = ({children}) => {
+    const { open } = useContext(OpenContext);
+    return open ?
+        <div className={itemContent}>
+            <div className={itemContentInner}>
+                {children}
+            </div>
+        </div> : <></>;
+};
+
+const EntryForm = () => {
+    const id = useId();
+    return <form action="#">
+               <ul className={items}>
+                   <li className={item}>
+                       <fieldset>
+                           <div>
+                               <label for={id}>Edit</label>
+                               <input id={id} type="text" />
+                           </div>
+                           <button>Submit</button>
+                       </fieldset>
+                   </li>
+                   <li className={item}>
+                       <fieldset>
+                           <button>Archive</button>
+                       </fieldset>
+                   </li>
+               </ul>
+           </form>;
+}
+
+const Entry = ({id}) =>
+<AccordionItem>
+    <AccordionItemSummary>{id}</AccordionItemSummary>
+    <AccordionItemContent>
+        <EntryForm />
+    </AccordionItemContent>
+</AccordionItem>;
+
+
+const Entries = () =>
+<Accordion>{ids.map(id => <Entry key={id} id={id} />)}</Accordion>;
+
 // FIXME make accordion?
-const TenPage = () =>
-<div>
-    <h1>Ten</h1>
-
-    <ul>
-        {
-            ids.map(id =>
-                <li key={id}>
-                    <div className={entry}>
-                        <details name="edit">
-                            <summary id={id} className={title}>{id}</summary>
-                            <form className={itemset} action="@">
-                                <ul className={items}>
-                                    <li className={item}>
-                                        <fieldset>
-                                            <span>
-                                                <label>Edit
-                                                    <input type="text" />
-                                                </label>
-                                            </span>
-                                            <button>Submit</button>
-                                        </fieldset>
-                                    </li>
-                                    <li className={item}>
-                                        <fieldset>
-                                            <button>Archive</button>
-                                        </fieldset>
-                                    </li>
-                                </ul>
-                            </form>
-                        </details>
-                    </div>
-                </li>)
-        }
-    </ul>
-
-    <h2>Archived</h2>
-    <ul>
-        {
-            archived.map(entry =>
-                <li key={entry}>{entry}</li>)
-        }
-    </ul>
-</div>;
+const TenPage = () => {
+    return <div>
+               <section>
+                   <h1>Ten</h1>
+                   <Entries />
+               </section>
+               <section>
+                   <h2>Archived</h2>
+                   <ul>
+                       {
+                           archived.map(entry =>
+                               <li key={entry}>{entry}</li>)
+                       }
+                   </ul>
+               </section>;
+           </div>;
+};
 
 
 export default TenPage;
