1a3710ce78e37fb291b466a9803cec6495884b95
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2025-07-18 11:53:06 -0700

    Play with PWA manifest

---
+++ ten-app/app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from "next";
+
+export const dynamic = "force-static";
+
+const manifest: () => MetadataRoute.Manifest = () => ({
+    name: 'Ten Things',
+    short_name: 'Ten',
+    description: 'A minimalist todo list app',
+    start_url: '/ten',
+    display: 'standalone',
+    background_color: '#FFFFFF',
+    theme_color: '#00FFFF',
+    icons: [
+        {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+        },
+        {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+        },
+    ]
+});
+
+export default manifest;
